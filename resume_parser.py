12cf725d7a85cd1c5a9fbd7a2d2648209fa763cd
Raed-Tanwar <raedtanwar72@gmail.com>
2026-09-20 12:06:54 +0530

    Update resume parser implementation

--- resume_parser.py
+++ resume_parser.py
@@ -9,7 +9,7 @@ load_dotenv()
 my_api_key=os.getenv("GROQ_API_KEY")
 
 if not my_api_key:
-    raise ValueError("API key kaha hai bhai")
+    raise ValueError("api error")
 
 client=Groq(api_key=my_api_key)
 model = "openai/gpt-oss-20b"
@@ -103,7 +103,7 @@ response=client.chat.completions.create(model=model, messages=messages, response
 answer=response.choices[0].message.content
 
 raw_json=answer
-# print(raw_json)
+
 
 
 
@@ -275,7 +275,7 @@ def read_resume(file_path):
 
 
 
-# lets do it now
+# comparing the standard schema with the resume which are made into our schema
 resume_folder = Path("resumes")
 all_results=[]
 for file_path in resume_folder.iterdir():
